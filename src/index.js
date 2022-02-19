1951cee261f5cd43a4e5c4915b8a4612b531bec3
Antoine CORDIER <acordier@chopin.local>
2022-02-19 21:23:32 +0100

    refactor: move to typescript

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export * from './meta-event';
-export * from './midi-event';
-export * from './midi-file';
-export * from './midi-message';
-export * from './midi-track';
-export * from './running-status';
-export * from './gm-instruments';
